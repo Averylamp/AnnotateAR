bb1ef3722a9a5dc4137581ed3afec65334ce5d24
Alden Lamp <alamp9008@gmail.com>
2018-03-31 18:36:40 -0400

    Adding more SCNs

--- AnnotatAR/MenuView.swift
+++ AnnotatAR/MenuView.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import SceneKit
 
 class MenuView: UIView{
     
@@ -15,21 +16,15 @@ class MenuView: UIView{
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
         self.frame = CGRect(x: 0, y: 0, width: size, height: size)
         
         
         
-            
-        
     }
-    
+        
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    
-    
-    
+
     
 }
