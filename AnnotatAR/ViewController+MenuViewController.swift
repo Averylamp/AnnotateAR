06d79acdb4a001b7165e306e93466c98bdbdfde2
Alden Lamp <alamp9008@gmail.com>
2018-03-31 15:30:11 -0400

    Expanding Menu

--- AnnotatAR/ViewController+MenuViewController.swift
+++ AnnotatAR/ViewController+MenuViewController.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+import UIKit
+
+extension ViewController: MenuViewControllerDelegate{
+    func toggleMenu(for state: menuState) {
+        
+        print("the menu will be toggled for: \(state == .close ? "Close" : "Open")")
+        if state == .close{
+            UIView.animate(withDuration: 1, animations: { [weak self] in
+                
+                self?.menuVC.view.frame = CGRect(x: CGFloat(0), y: (self?.view.frame.height)! - MenuViewController.heightOfExpandButton, width: (self?.view.frame.height)!, height: MenuViewController.heightOfView)
+                
+            })
+        }else{
+            UIView.animate(withDuration: 1, animations: { [weak self] in
+                self?.menuVC.view.frame = CGRect(x: CGFloat(0), y: (self?.view.frame.height)! - MenuViewController.heightOfView, width: (self?.view.frame.height)!, height: MenuViewController.heightOfView)
+            })
+        }
+    }
+}
+
+
